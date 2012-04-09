1ae6e1f8694f882c233d50539cfa02bf06a3b62c
Mike Yumatov <mike@yumatov.org>
2012-04-09 16:21:00 +0400

    Add docstrings for snippets.metadata_parser

--- snippets/metadata_parser.py
+++ snippets/metadata_parser.py
@@ -2,14 +2,23 @@ import re
 from pygments.token import Comment
 
 
+#: A regular expression object to find all metadata
+#: params in source code header comments.
 metadata_re = re.compile(r'!(\w+):\s*(.+)$', re.M)
 
 
 def parse_comment_metadata(comment):
+    """Find all metadata params in provided comment string. Found params are
+    returned as dictionary.
+    """
     return dict(metadata_re.findall(comment))
 
 
 def parse_metadata(tokens):
+    """Find all metadata params in provided tokens. A two-tuple is returned
+    with found params as first item and comments without metadata as second
+    item. Foind params are returned as dictionary.
+    """
     comments = []
     metadata = {}
     is_significant = True
