fe29d77e6e33640de22c92ce54da248b01a553f6
Mike Yumatov <mike@yumatov.org>
2012-04-09 16:31:01 +0400

    Add docstrings to snippets.token_splitter

--- snippets/token_splitter.py
+++ snippets/token_splitter.py
@@ -3,10 +3,14 @@ from pygments.token import Comment, Text
 
 
 def is_token_match(token):
+    """Check if provided token is a header token. Token is a header token, if
+    it contains a comment or only whitespace characters.
+    """
     return token[0] is Comment or token[0] is Text and token[1].isspace()
 
 
 def split(tokens):
+    """Split provided tokens to header tokens and source code tokens."""
     tokens = tuple(tokens)
     header_tokens = []
     for token in tokens:
