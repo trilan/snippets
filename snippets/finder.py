a04adfcdc202eded3d50969ec006b18d370a8a26
Mike Yumatov <mike@yumatov.org>
2012-04-09 16:17:33 +0400

    Add docstrings for snippets.finder

--- snippets/finder.py
+++ snippets/finder.py
@@ -2,6 +2,7 @@ import os
 
 
 def find(root):
+    """Yield all found filepaths in provided directory recursively."""
     for current, dirs, files in os.walk(root):
         for file in files:
             yield os.path.join(current, file)
