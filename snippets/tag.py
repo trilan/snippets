cc704e588ec69705cfa9b6811147c1d4964719d8
Mike Yumatov <mike@yumatov.org>
2012-04-11 14:27:22 +0400

    Improve tags functionality

--- snippets/tag.py
+++ snippets/tag.py
@@ -1,9 +1,27 @@
 class Tag(set):
+    """Represents a snippets tag. Tags with the same slug are the same objects.
+    So, e.g., tags with names ``Django``, ``django`` and ``DJANGO`` will share
+    the same set of snippets.
+    """
+
+    registry = {}
+
+    def __new__(cls, name):
+        slug = name.lower()
+        if slug in cls.registry:
+            return cls.registry[slug]
+        return super(Tag, cls).__new__(cls, name)
 
     def __init__(self, name):
-        self.name = name
-        self.slug = name.lower()
-        super(Tag, self).__init__()
+        slug = name.lower()
+        if slug not in self.registry:
+            self.name = name
+            self.slug = slug
+            super(Tag, self).__init__()
+            self.registry[slug] = self
+
+    def __repr__(self):
+        return u'<Tag %s>' % self.name
 
     def get_relpath(self):
         return 'tags/{slug}.html'.format(slug=self.slug)
---
+++ tests/test_tag.py
@@ -0,0 +1,22 @@
+import mock
+from unittest2 import TestCase
+from snippets.tag import Tag
+
+
+class TagTests(TestCase):
+
+    def setUp(self):
+        Tag.registry.clear()
+
+    def test_does_not_create_new_tags_with_the_same_name(self):
+        self.assertIs(Tag('Django'), Tag('Django'))
+        self.assertIs(Tag('Django'), Tag('django'))
+
+    def test_saves_snippets_between_objects(self):
+        tag = Tag('Django')
+        tag.add(mock.sentinel.snippet1)
+        tag.add(mock.sentinel.snippet2)
+        self.assertItemsEqual(Tag('django'), [mock.sentinel.snippet1, mock.sentinel.snippet2])
+
+    def test_relpath(self):
+        self.assertEqual(Tag('Django').get_relpath(), 'tags/django.html')
