8ad795f86e16209007537cbf47a3466733653e2d
Mike Yumatov <mike@yumatov.org>
2012-04-12 17:02:29 +0400

    Fix cli argument name for repository path

--- snippets/__main__.py
+++ snippets/__main__.py
@@ -7,13 +7,13 @@ from .repository import Repository
 
 def run(args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--source', default='snippets')
+    parser.add_argument('-r', '--repository', default='snippets')
     parser.add_argument('-o', '--output', default='output')
     parser.add_argument('-t', '--theme')
     args = parser.parse_args(args)
 
     repository = Repository()
-    repository.add_repopath(args.source)
+    repository.add_repopath(args.repository)
     generator = Generator(repository, args.theme)
     generator.generate(args.output)
 
